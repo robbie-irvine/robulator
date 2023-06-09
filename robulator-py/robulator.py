22675577f8be3bb6ccaa1c31120800f62cde57ad
Robbie Irvine <rni2000@hw.ac.uk>
2023-06-09 14:54:25 +0100

    Created lexer for addition only

--- robulator-py/robulator.py
+++ robulator-py/robulator.py
@@ -1,2 +1,20 @@
 from .ply import lex
 from .ply import yacc
+
+tokens = (
+    'NUMBER',
+    'PLUS',
+)
+
+t_ignore_SPACE = r' '
+
+t_PLUS  = r'\+'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+lexer = lex()
+
+input("Enter your calculation here:\n")
