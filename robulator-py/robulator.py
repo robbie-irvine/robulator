87755d32a3f5a0400167ad0f2e9d64c658885204
Robbie Irvine <rni2000@hw.ac.uk>
2023-06-10 11:08:15 +0100

    Lexer runs with input

--- robulator-py/robulator.py
+++ robulator-py/robulator.py
@@ -1,12 +1,12 @@
-from .ply import lex
-from .ply import yacc
+from ply import lex
+from ply import yacc
 
 tokens = (
     'NUMBER',
     'PLUS',
 )
 
-t_ignore_SPACE = r' '
+t_ignore_SPACE = r' \t'
 
 t_PLUS  = r'\+'
 
@@ -15,8 +15,14 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
-lexer = lex()
+lexer = lex.lex()
 
 calc = input("Enter your calculation here:\n")
 
-# TODO: make lexer run with input
+lexer.input(calc)
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
